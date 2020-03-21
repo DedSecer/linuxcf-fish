dd5bae51674ea80c3f65983f800f52f861eef142
DedSecer <z2465216809qq.com@icloud.com>
2020-03-21 16:22:14 +0800

    my fish config

---
+++ config.fish
@@ -0,0 +1,34 @@
+# Put system-wide fish configuration entries here
+# or in .fish files in conf.d/
+# Files in conf.d can be overridden by the user
+# by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
+
+# This file is run by all fish instances.
+# To include configuration only for login shells, use
+# if status --is-login
+#    ...
+# end
+# To include configuration only for interactive shells, use
+# if status --is-interactive
+#   ...
+# end
+
+set PATH $PATH ~/Scripts
+set EDITOR /usr/bin/nvim
+set BROWSER /usr/bin/google-chrome-stable
+
+function fish_greeting
+end
+
+alias pyenv-init 'status --is-interactive; and source (pyenv init -|psub)'
+alias ra 'ranger'
+alias vim 'nvim'
+alias sudo 'sudo -s' 
+alias rm 'trash-put'
+
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/dedsecer/.local/share/omf/themes/clearance/fish_prompt.fish
