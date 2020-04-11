2311c0c0510822e5a17b4648519e9f0ffd945304
DedSecer <ds@tsarch.localdomain>
2020-04-11 09:35:59 +0800

    fix ud_ln.sh

--- ud_ln.sh
+++ ud_ln.sh
@@ -4,8 +4,9 @@
 
 fish_path=~/.config/fish
 ln_cmd='ln -i'
+func_path=functions
 
-git pull
+#git pull
 
 mkdir -p $fish_path
 
@@ -15,4 +16,7 @@ mkdir -p $fish_path/conf.d
 ${ln_cmd} ./conf.d/dracula.fish $fish_path/conf.d/
 
 mkdir -p $fish_path/functions/
-${ln_cmd} ./functions/fish_prompt.fish $fish_path/functions/
+for file in `ls ./${func_path}`  
+do
+	${ln_cmd} "./${func_path}/${file}"  "${fish_path}/${func_path}/"
+done	
