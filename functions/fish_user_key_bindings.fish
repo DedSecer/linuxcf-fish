906babb107a396a4aa4726cdee0eb40a4a347b9f
DedSecer <z2465216809qq.com@icloud.com>
2020-08-08 11:47:28 +0800

    changed keydinging

---
+++ functions/fish_user_key_bindings.fish
@@ -0,0 +1,7 @@
+function fish_user_key_bindings
+	bind -e -M insert --preset \ee
+	bind -e --preset \ee
+	bind -e -M insert --preset  \ep
+	bind -e -M visual --preset  \ep
+	bind -e --preset  \ep
+end
