0226cc24fb08d06be3acbbbd97a85f4e98bceae4
Ded_Secer <z2465216809qq.com@icloud.com>
2021-06-08 19:19:57 +0800

    fixed proxy

--- functions/set_proxy.fish
+++ functions/set_proxy.fish
@@ -1,4 +1,13 @@
 function set_proxy
-    set -Ux http_proxy $argv
-    set -Ux https_proxy $argv
+    set -eg http_proxy
+    set -eg https_proxy
+
+    if test -n "$argv"
+        set -Ux http_proxy $argv
+        set -Ux https_proxy $argv
+    else
+        set -eU http_proxy
+        set -eU https_proxy
+    end
+
 end
