9ae1d715343e6afcb4e3ccf594a584297f1af469
Ded_Secer <z2465216809qq.com@icloud.com>
2023-06-24 15:29:30 +0800

    update set_proxy

--- functions/set_proxy.fish
+++ functions/set_proxy.fish
@@ -1,13 +1,10 @@
 function set_proxy
-    set -eg http_proxy
-    set -eg https_proxy
-
     if test -n "$argv"
-        set -Ux http_proxy $argv
-        set -Ux https_proxy $argv
+        set -gx http_proxy $argv
+        set -gx https_proxy $argv
     else
-        set -eU http_proxy
-        set -eU https_proxy
+        set -eg http_proxy
+        set -eg https_proxy
     end
 
 end
